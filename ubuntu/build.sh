7a67cc0ee0516c870f5058149b9824fdec42d8ed
yukke42 <muramatsuyusuke9542@gmail.com>
2017-10-05 14:34:03 +0900

    add README

--- ubuntu/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source container.conf
-
-printf "\e[32mCreate a new docker iamge\n\
-  username: ${CONTAINER_USER}\n\
-  image_repository: ${IMAGE_REPOSITORY}:${IMAGE_TAG}\e[m\n"
-
-docker build -t ${IMAGE_REPOSITORY}:${IMAGE_TAG} .
--- ubuntu/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source container.conf
-
-docker run -it --rm \
-  -h $CONTAINER_HOST \
-  $IMAGE_REPOSITORY:$IMAGE_TAG $COMMAND
